778f08f0fa633e9fdf507becd6e29d3d5a5f29a4
Kennedythacode <johnkennedyx1212@gmail.com>
2020-02-13 15:07:59 -0800

    renamed folder and python file to lightdl_movies_downloader

---
+++ lightdl_movies_downloader.py
@@ -0,0 +1,78 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import random
+import wget
+#from selenium import webdriver
+#from selenium.webdriver import Chrome
+import time
+import json
+
+
+
+def download_movies():
+    while True:
+        session = requests.Session()
+        list_of_user_agents = [
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+            #Firefox
+            'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+            'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+            'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+            'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko',
+            'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+            'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)',
+            'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+            'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+            'Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko',
+            'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+            'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+            'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)'
+             
+            ]
+        #RANDOM SELECTION OF USER AGENTS TO PREVENT EASY BANNING BY THE SITE
+        user_agent = random.choice(list_of_user_agents)
+        headers = {'User-Agent':user_agent}
+        print('Welcome to MoviesOget')
+        movie_name = input('What movie would you like to download?:::::::')
+        url = 'https://lightdlmovies.blogspot.com/search?q={}'.format(movie_name)
+               
+        get_url = session.get(url, headers=headers)
+        soup = bs(get_url.text, 'html.parser')
+        div = soup.find('div',{'class':'Blog'})
+        div_2 = div.find('div', {'class':'post-body'})
+        a_href = div_2.find_all('a')
+        download_links = {}
+        count = 0
+        del a_href[0]
+        for dl in a_href:
+            link = dl['href']
+            download_links[count] = link
+            count += 1
+        count = 0 
+        for dl in download_links:
+            print(f'{count} ---->>>>> {download_links[count]} \n')
+            count += 1
+        print('ENTER THE NUMBER TO DOWNLOAD THE QUALITY YOU WISH')
+        while True:
+            try:
+                user_quality = int(input(">>:"))
+                movie = wget.download(download_links[user_quality], bar=bar_thermometer)
+            except( ValueError):
+                print("ENTER A WHOLE NUMBER NOT STRING OR DECIMAL")
+                continue
+download_movies()
+
+
+
+def something():
+    pass
